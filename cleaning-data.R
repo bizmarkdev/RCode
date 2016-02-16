9386c9783ba95f1bf97f12cf73a55454041a9436
Hicks <mhicks@mhicks.local>
2016-02-16 15:55:57 -0600

    changed cleaning-data.R

--- cleaning-data.R
+++ cleaning-data.R
@@ -11,3 +11,17 @@ if (pollutant == 'sulfate') {
   pollution_data <- pollution_data[complete.cases(pollution_data),]
   pollution_mean <- "select either sulfate or nitrate"
 }
+
+# remove NA values
+x<-c(1,2,NA,4,NA,5)
+x
+bad<-is.na(x)
+bad
+x[!bad]
+
+x<-c(1,2,NA,4,NA,5)
+y<-c("a","b",NA,"d",NA,"f")
+good<-complete.cases(x,y)
+x[good]
+y[good]
+
